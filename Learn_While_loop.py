15149af6a760ebb7e79032b119fe8c5e952d08c4
marskingx <shamangels@gmail.com>
2023-02-17 20:26:00 +0800

    無限回圈練習

--- Learn_While_loop.py
+++ Learn_While_loop.py
@@ -11,3 +11,9 @@ while True:  # 當x小於10的時候，進入迴圈
         print('請輸入1/2/0')
 
 print('我出來了')
+
+while True:  # 當x小於10的時候，進入迴圈
+    name = input('請輸入你所有朋友的姓名：')
+    if name == '離開':
+        print('離開輸入模式')
+        break
