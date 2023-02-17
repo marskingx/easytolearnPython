99f7c971be76708e6f2e448c57885d45bf92a609
marskingx <shamangels@gmail.com>
2023-02-17 15:16:53 +0800

    把學習內容依檔案分類

--- day2.py
+++ day2.py
@@ -1,10 +1,14 @@
 # 第二天學習的內容
+import food as food
+
 rain = input('請問今天有下雨嗎?')
 if rain == '有':
     print('撐傘出門')
     print('買一包餅干')
     print('在家看電影')
-
+    if  < food:
+        print('想想看吃什麼才好')
+        
 while rain == '無':
     print('撐傘出門')
     print('買一包餅干')
