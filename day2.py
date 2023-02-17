8bb2251b596d9e1c26dbfecbba96274f424b167d
marskingx <shamangels@gmail.com>
2023-02-17 15:31:24 +0800

    把學習內容依檔案分類

--- day2.py
+++ day2.py
@@ -6,9 +6,8 @@ if rain == '有':
     print('撐傘出門')
     print('買一包餅干')
     print('在家看電影')
-    if  < food:
+    if food < 3:
         print('想想看吃什麼才好')
-        
 while rain == '無':
     print('撐傘出門')
     print('買一包餅干')
