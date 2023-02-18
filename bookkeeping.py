c697222482aa4a524c77be24319d647d30012255
marskingx <shamangels@gmail.com>
2023-02-19 06:57:39 +0800

    練習建立一個記帳程式，能中讀取舊檔案，並透過清單整理單內容，再讓使用者輸入新資訊，並且把程式內容存入檔案

--- bookkeeping.py
+++ bookkeeping.py
@@ -1,12 +1,29 @@
+import xml
+#  打開同一個檔案
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue  # 如果遇到商品,價格這個字串的時候就跳到下一個迴圈
+        name, price = line.strip().split(',')
+        products.append([name, price])  # 把p放入products
+print(products)
+
+# 讓使用者輸入
 while True:
     name = input('請輸入商品名稱：')
     if name == 'q':
         break
     price = input('請輸入商品價格：')
+    price = int(price)
     products.append([name, price])  # 把p放入products
 
-print(products[1][1])
-
+# 印出購買記錄
 for p in products:
     print(p[0], '價格是', p[1])
+
+# 寫入檔案中並儲存
+with open('products.csv', 'w', encoding='utf-8') as f:
+    f.write('商品,價格\n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
